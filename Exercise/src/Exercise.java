4d36e0e635a29a713f4095b96a56ef5de61cdbb8
Peter Schoellhorn <peterschoellhorn@gmail.com>
2020-07-11 16:37:43 +0200

    creating main/exercise class + constructor

---
+++ Exercise/src/Exercise.java
@@ -0,0 +1,19 @@
+public class Exercise {
+
+    // field declaration //
+
+    String name;
+    String description;
+    int duration;
+    String position;
+
+    // Constructor //
+
+    Exercise(String name, String description, int duration, String position){
+
+        this.name = name;
+        this.description = description;
+        this.duration = duration;
+        this.position = position;
+    }
+}
---
+++ Exercise/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        Exercise pushUp = new Exercise("Push Up", "",30, "floor");
+        System.out.println("Ausgabe aus der main()-Methode");
+    }
+}
